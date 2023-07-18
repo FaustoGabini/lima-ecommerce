d07abe068c8ad80994a299c3a1087315122ec971
Fausto Gabini <fausgabini@gmail.com>
2023-07-18 20:53:54 -0300

    Update constants.js

--- src/app/constants.js
+++ src/app/constants.js
@@ -2,7 +2,7 @@ export const INFORMATION = {
   avatar:
     "https://res.cloudinary.com/dgq6qxh4k/image/upload/v1689696148/Lima_oedmeh.png",
   banner:
-    "https://static.vecteezy.com/system/resources/previews/010/049/487/non_2x/summer-banner-with-bikini-girl-on-the-beach-on-the-swing-and-with-tropical-leaves-illustration-vector.jpg",
+    "https://lh3.googleusercontent.com/pw/AIL4fc_UhXupbvScTaSRUq14ZYY2qUeLbqHMvNAXfDp1Xbg0lzhlIVQx6NoYpVnQk3rBJwVN0luKVOmlkG4Fu9ZV2oXQsyn-TJopbCeU3ASIAIb5v4K_XgzTnPcZdfEf9Z0M6cOQN6ys6mr18Up40ojIf1mQU7h4mcT6SnAgjfiG91CvT8cYixbI8N778hPmlGGVkCW8dNWayTiYMfXenGbugbhaCJ1tRlrSp9W3431QE3p7qGH1jXSe0yVud-Yi2EPxw1mL57x0p67rtR5vr9UlGbpaY9fDIrG_dTtRtIFNmCPRyYefPM4f7b_Gv-JIiYwi9wyww-RzP51qlZ8gryQywQHfzFM_FI_TNpSvd_btxNfYXDsd1X9vod95ZfB736Aoih6qLFSp3cSwo2w2xQRi89IenZGyFA-7tPDa3fRRJIIx2J-EdwckYS06ZzXNZtsAKI_WKT29BeDYfLtv4DarUEkpjD4U8JSX1dOjwz4g-CVS1TBkBL4gFqxxNUEj8qhVn-073bn-nS_AIXlD3ax7hDyaPFAc3ejqRnStuvCl9jeoxO1D90N0A237X2JE7sduS2QEpOCSZrBM6MW2W1fu_Jkbrjz0mBv2b4dEoYWBGp4YfXf2t7Yh4xIm2PLEjOOumhpjafeO-Jop_d25Lq_lUCR0Iab59xzfEXJFZlYZfdjbbbAgs8btls3z8AW-Nd8Ur1I6t2GivCNxyYdoj0xY9EgV_Qyk2GCiHL5I_v2YrtqkyhdEm1dgS6jIulgCYDq3faXxs5dwmG7_TzaYMr58f-TVmNjNtAPJlh49POseL9YC-jE546icV0CUZ55FjVL1urwJ8V0WUnCpYm3pylDu5ZElcxAZx4luXW2dhcjwTIMV-zLGs6LYzvLjOZJuWbZ3YCGaKDw8m5hJUrI0VvzE23cvk2A=w1184-h592-s-no?authuser=0",
   title: "Lima Boutique",
   description: "",
   phone: "5493404438300",
