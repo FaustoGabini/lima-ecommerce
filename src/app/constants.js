72fd1bb2f9ebbc10f8dee84459ca9b53ddd40ee5
Fausto Gabini <fausgabini@gmail.com>
2023-07-20 19:50:33 -0300

    Update constants.js

--- src/app/constants.js
+++ src/app/constants.js
@@ -2,7 +2,7 @@ export const INFORMATION = {
   avatar:
     "https://res.cloudinary.com/dgq6qxh4k/image/upload/v1689696148/Lima_oedmeh.png",
   banner:
-    "https://res.cloudinary.com/dw5swolsy/image/upload/v1689727022/Pastel_Rosa_B%C3%ADgaro_y_Blanco_Salones_Reapertura_Comercial_Horizontal_P%C3%B3ster_xes9s3.png",
+    "https://res.cloudinary.com/dw5swolsy/image/upload/v1689892519/Pastel_Rosa_B%C3%ADgaro_y_Blanco_Salones_Reapertura_Comercial_Horizontal_P%C3%B3ster_xes9s3_tcvt6l.png",
   title: "Lima Boutique",
   description: "",
   phone: "5493404438300",
